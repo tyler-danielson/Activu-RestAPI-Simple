3b454d151a026dc38ceac99e725c726ca178e618
tyler-danielson <38443137+tyler-danielson@users.noreply.github.com>
2023-10-04 08:23:32 -0600

    Delete Activu-RestAPI-Simple directory

--- Activu-RestAPI-Simple/properties.lua
+++
@@ -1,7 +0,0 @@
-props={
-  --{Name="Plugin Mode",Type="enum",Choices={"Simple","Touchpanel"},Value="Simple"},
-  {Name = "Number of Layouts",Type = "integer",Min = 0,Max = 25,Value = 5},
-  {Name = "Number of Scripts",Type = "integer",Min = 0,Max = 25,Value = 5},
-  {Name = "Number of Sources",Type = "integer",Min = 0,Max = 25,Value = 5},
-  {Name ="Debug Print",Type = "enum",Choices = {"None", "Tx/Rx", "Tx", "Rx", "Function Calls", "All"},Value = "None"},
-}
