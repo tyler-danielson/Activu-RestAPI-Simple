4c676c9098b49766f834b62173e228d880285ad3
tyler-danielson <38443137+tyler-danielson@users.noreply.github.com>
2023-10-04 08:17:49 -0600

    Add files via upload

---
+++ info.lua
@@ -0,0 +1,8 @@
+PluginInfo = {
+  Name = "Activu~Basic Control",
+  Version = "0.3",
+  BuildVersion = "1.1.0.1758",
+  Id = "Activu RestAPI Simple",
+  Author = "Tyler Danielson",
+  Description = "A plugin to interface with Activu RestAPI.  Written by Tyler Danielson for Activu Corp."
+}
